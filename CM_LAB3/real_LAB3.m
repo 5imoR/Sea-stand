5197699b1ae3cadb6f6ea4d0de188e336d201d9d
esterfoschi <ester.foschi@studenti.unipd.it>
2026-05-07 16:04:40 +0200

    .

--- CM_LAB3/real_LAB3.m
+++ CM_LAB3/real_LAB3.m
@@ -1,89 +1,110 @@
-%%% LABORATORIO 3 RESONANT LOAD POSITION CONTROLLER
+%% LABORATORY 3 - RESONANT LOAD POSITION CONTROLLER
 clear all
 clc
 
 %% LOAD PARAMETERS
-load ('./../resonant_params.mat'); % motor and load parameters
-load ('./../est_param.mat','B_eq_est','tau_sf_est'); % estimated parameters ( B_eq, tau_sf)
-%load ('./../est_param_resonant.mat'); % estimated parameters ( Bb_est, k_est)
-Beq=B_eq_est;
-tau_sf=tau_sf_est;
-Req = mot.R + sens.curr.Rs;
-Jeq = mot.J + mld.Jh / (gbox.N^2); 
-Ts = 0.001; % Sampling time
-t1=0.7;
-t0=0.2;
+load ('./../resonant_params.mat');                      % Nominal motor and load parameters
+load ('./../est_param.mat','B_eq_est','tau_sf_est');    % Estimated friction parameters 
+load ('./../est_param_resonant.mat');                   % Estimated elastic joint parameters
 
+Beq = B_eq_est;
+tau_sf = tau_sf_est;
+Req = mot.R + sens.curr.Rs;         % Total equivalent resistance
+Jeq = mot.J + mld.Jh / (gbox.N^2);  % Equivalent inertia seen at motor side
+
+Ts = 0.001;                         % Sampling time [s]
+t1 = 0.7;
+t0 = 0.2;
+
+%% CHOICE OF PID 
 
 %% PID CONTROLLER & ANTI-WINDUP
 % Specifications for PID design
-alpha= 4;
-Mp = 0.3;
-ts = 0.85;
-% PID parameters (found using a functon LAB0_design_controller)
-% Plant transfer function U --> thh
-num = [(drv.dcgain*mot.Kt*mld.Jb)  (drv.dcgain*mot.Kt*mld.Bb)  (drv.dcgain*mot.Kt*mld.k)];
-den = [(gbox.N*Req*Jeq*mld.Jb)  gbox.N*(Req*Jeq*mld.Bb+Req*mld.Jb*Beq+mot.Kt*mot.Ke*mld.Jb) gbox.N*(mot.Kt*mot.Ke*mld.Bb+Req*Beq*mld.Bb+Req*mld.k*(Jeq+mld.Jb/(gbox.N^2))) gbox.N*(mot.Kt*mot.Ke*mld.k+Req*mld.k*Beq+Req*mld.k*mld.Bb/(gbox.N^2)) 0];
-P_s = tf(num,den);
-[Kp, Ki, Kd, delta, w_n] = LAB3_design_controller(P_s, Mp, ts, alpha);
-% Real derivative design 
-% Find Tl for "real derivative" (High Pass Filter)
-Tl = 1 / (10 * w_n);
-% AntiWindup parameters
-Tw=ts/5;
-Kw=1/Tw;
+alpha= 4;   % Ratio between Ti and Td
+Mp = 0.3;   % Maximum overshoot (30%)
+ts = 0.85;  % Settling time [s]
 
-%% STATE-SPACE CONTROLLER WITH EIGEN ALLOCATIONS METHOD
-% High pass filter real derivaties 
-wc=2*pi*50;
-deltac=1/sqrt(2);
-% State space model
-A=[0     0                                 1                                              0; 
-   0     0                                 0                                              1; 
-   0     (mld.k/(gbox.N^2*Jeq))            -(1/Jeq)*(Beq+((mot.Ke*mot.Kt)/Req))             0; 
-   0     -mld.k/mld.Jb-mld.k/(Jeq*gbox.N^2)  -(mld.Bb/mld.Jb)+(1/Jeq)*(Beq+mot.Ke*mot.Kt/Req) -mld.Bb/mld.Jb];
-B=[0;
-   0;
-   (mot.Kt*drv.dcgain)/(gbox.N*Jeq*Req);
-   -(mot.Kt*drv.dcgain)/(gbox.N*Jeq*Req)];
-Bd=[0;
-   0;
-   1/((gbox.N^2)*Jeq);
-   -1/((gbox.N^2)*Jeq)];
+% Plant transfer function U --> thh
+numP= [(drv.dcgain*mot.Kt*mld.Jb)  (drv.dcgain*mot.Kt*Bb_est)  (drv.dcgain*mot.Kt*k_est)];
+denP = [(gbox.N*Req*Jeq*mld.Jb)  gbox.N*(Req*Jeq*Bb_est+Req*mld.Jb*Beq+mot.Kt*mot.Ke*mld.Jb) gbox.N*(mot.Kt*mot.Ke*Bb_est+Req*Beq*Bb_est+Req*k_est*(Jeq+mld.Jb/(gbox.N^2))) gbox.N*(mot.Kt*mot.Ke*k_est+Req*k_est*Beq+Req*k_est*Bb_est/(gbox.N^2)) 0];
+
+P_s = tf(numP,denP);
+
+% Compute PID gains, required damping factor, and natural frequency
+[Kp, Ki, Kd, delta, w_n] = LAB3_design_controller(P_s, Mp, ts, alpha);
+
+% Real derivative filter design 
+Tl = 1 / (10 * w_n);
+
+% AntiWindup parameters
+Tw = ts / 5;
+Kw = 1 / Tw;    % Anti-windup gain
+
+
+%% STATE-SPACE CONTROLLER WITH EIGENVAlUES ALLOCATIONS METHOD
+% High pass filter for real derivaties (representative of the observer) 
+wc = 2*pi*50;         % Cut.off frequency [rad/s]
+deltac = 1/sqrt(2);   % Damping factor
+
+% State space model of the plant (State vector x' = [th_h, th_d, w_h, w_d]')
+A = [0     0                                 1                                              0; 
+     0     0                                 0                                              1; 
+     0     (k_est/(gbox.N^2*Jeq))            -(1/Jeq)*(Beq+((mot.Ke*mot.Kt)/Req))             0; 
+     0     -k_est/mld.Jb-k_est/(Jeq*gbox.N^2)  -(Bb_est/mld.Jb)+(1/Jeq)*(Beq+mot.Ke*mot.Kt/Req) -Bb_est/mld.Jb];
+
+B = [0; 0; (mot.Kt*drv.dcgain)/(gbox.N*Jeq*Req); -(mot.Kt*drv.dcgain)/(gbox.N*Jeq*Req)];
+
+Bd=[0; 0; 1/((gbox.N^2)*Jeq); -1/((gbox.N^2)*Jeq)];
+
 C=[1 0 0 0];
-% poles (lambda)
-phi=atan(sqrt(1-delta^2)/(delta));
-p1 = w_n*exp(1i*(-pi+(phi)));
+
+% Target poles for the closed-loop system
+phi = atan(sqrt(1-delta^2)/(delta));
+p1 = w_n*exp(1i*(-pi+(phi)));     % First pair of dominant poles
 p2 = conj(p1);
-p3 = w_n*exp(1i*(-pi+(phi/2)));
+p3 = w_n*exp(1i*(-pi+(phi/2)));   % Second pair of faster poles
 p4 = conj(p3);
-poles = [p1, p2, p3, p4]; % vector of poles
-% state feedback matrix
-K = place(A, B, poles); 
-% feedforward matrices
+poles = [p1, p2, p3, p4];         % Poles vector
+
+% State feedback matrix
+K = place(A, B, poles);  
+
+% Feedforward matrices
 S = [A B; C 0];
-N = inv(S)*[0; 0; 0; 0; 1];
-Nx = N(1:4); % state feedback matrix
-Nu = N(5);   % input feedback matrix
+N = S \ [0; 0; 0; 0; 1];
+Nx = N(1:4);    % state feedback matrix
+Nu = N(5);      % input feedback matrix
 
 %% STATE SPACE CONTROLLER WITH LQR METHOD
 sysG=ss(A,B,C,0);
 sysGp=ss(-A,-B,C,0);
-%figure
-%rlocus(sysG*sysGp)
-%hold on
-%plot([-3/ts -3/ts], [-1 1]*(-3/ts*tan(phi)), '--', 'Color', [0 0.4470 0.7410])
-%plot([-100 -3/ts], [-100 -3/ts]*tan(phi), '--', 'Color', [0 0.4470 0.7410])
-%plot([-100 -3/ts], [-100 -3/ts]*-tan(phi), '--', 'Color', [0 0.4470 0.7410])
-%grid on
-%hold off
-r=1/4.59e3;
+
+% -- LQR via Symmetric Root Locus -- 
+% Plot SRL to analyze how closed-loop eigenvalues move as input cost (r) decreases
+figure(1)
+rlocus(sysG*sysGp)
+hold on
+% Plot performance boundaries based on specs (Settling time and Overshoot)
+plot([-3/ts -3/ts], [-1 1]*(-3/ts*tan(phi)), '--', 'Color', [0 0.4470 0.7410])
+plot([-100 -3/ts], [-100 -3/ts]*tan(phi), '--', 'Color', [0 0.4470 0.7410])
+plot([-100 -3/ts], [-100 -3/ts]*-tan(phi), '--', 'Color', [0 0.4470 0.7410])
+grid on
+axis([-60 60 -60 60]);
+title('Symmetric Root Locus (LQR Design)')
+hold off
+
+% Gain 1/r chosen graphically to keep poles inside the desired performance region
+r = 1/4.59e3;
+
+% Compute LQR gain matrix (Standard cost function with Q = C'*C)
 K_lqr=lqr(sysG, C'*C, r);
 
-% Another versione of lqr
-Q=[1/(0.3*50*pi/180)^2 0           0  0;
-   0                   1/(pi/36)^2 0  0;
-   0                   0           0  0;
-   0                   0           0  0;];
-R=1/100;
-K_lqr2=lqr(sysG,Q,R);
+% -- LQR via Bryson's Rule (Cost Weights tuning) ---
+Q = [1/(0.3*50*pi/180)^2 0           0  0;
+     0                   1/(pi/36)^2 0  0;
+     0                   0           0  0;
+     0                   0           0  0;];
+R = 1/100;
+
+% Compute LQR gain matrix using Bryson's Rule weights
+K_lqr2 = lqr(sysG,Q,R);
--- CM_LAB3/real_estimation.m
+++ CM_LAB3/real_estimation.m
@@ -2,13 +2,15 @@
 %clear all
 clc
 
-% Estimation of w_n and delta
 load ('./../resonant_params.mat','mld'); % motor and load parameters
+
 wn = sqrt(mld.k/mld.Jb);
 delta = mld.Bb/(2*sqrt(mld.Jb*mld.k));
 w = wn*sqrt(1-delta^2);
 Ts = 0.001;
+
 %%
+% Estimation of natural frequency and damping factor
 thd_abs = abs(out.thd.signals.values);
 t = out.thd.time;
 
@@ -33,20 +35,25 @@ t = out.thd.time;
 delta_tk = pi / w;
 minimum_samples = round((0.8 * delta_tk) / Ts); % minimum number of samples between to peaks obtained exploiting eqn (68): tk = k*pi-phi/w so t_k+1-t_k = pi/w 
 
+% Find peaks (absolute values) and corresponding time instances 
 [thd_k_vector, index_peaks] = findpeaks(thd_abs, 'MinPeakHeight', 1,'MinPeakDistance', minimum_samples); % peaks must be at least of 1 deg and at least after tot samples
-tk_vector = t(index_peaks);
-%%
-M = length(thd_k_vector);
-Y = log(thd_k_vector);
-phi = [-(0:M-1)',ones(M,1)];
+tk_vector = t(index_peaks); 
 
-theta_LS = phi\Y;
-eta_est = theta_LS(1);
+% Linear LS fitting of data (log)
+M = length(thd_k_vector);       % number of peaks
+Y = log(thd_k_vector(:));       % column vector of log(abs(peak_value))  (Y = phi*theta)
+phi = [-(0:M-1)', ones(M,1)];   % regressor matrix
+theta_LS = phi\Y;               % LS solution (slope and intercept) 
 
-delta_est = eta_est/sqrt(pi^2+eta_est^2);
+eta_est = theta_LS(1);                      % estimate of the loarithmic decrement
+delta_est = eta_est/sqrt(pi^2+eta_est^2);   % estimate of the damping factor
+Tk = tk_vector(2:M) - tk_vector(1:M-1);     % vector of measured time intervals between consecutive peaks 
+wk_est = pi./Tk;                            % vector of estimates of the frequency for every time interval Tk 
+w_est = mean(wk_est);                       % averaged estimate of the frequency 
+wn_est = w_est/sqrt(1-delta_est^2);         % estimate of the natural frequency
 
-Tk = tk_vector(2:M) - tk_vector(1:M-1);
-wk_est = pi./Tk;
+% Estimation of beam viscous friction coefficient and elastic joint stiffness
+Bb_est = mld.Jb*2*delta_est*wn_est;   % estimate of the beam friction coefficient
+k_est = mld.Jb*wn_est^2;              % estimate of the elastic joint stiffness
 
-w_est = mean(wk_est);
-wn_est = w_est/sqrt(1-delta_est^2);
+save('est_param_resonant.mat', 'Bb_est', 'k_est')

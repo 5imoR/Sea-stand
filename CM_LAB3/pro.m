698c9f1fa0aa49f6bee2b4c24a8ff1475deefd5c
esterfoschi <ester.foschi@studenti.unipd.it>
2026-05-07 13:37:53 +0200

    .

---
+++ CM_LAB3/pro.m
@@ -0,0 +1,8 @@
+vector = [0:0.1:10];
+it = 0;
+for i = vector
+    disp(it+1)
+    disp(i)
+    disp('')
+    it = it+1;
+end
--- CM_LAB3/real_estimation.m
+++ CM_LAB3/real_estimation.m
@@ -8,36 +8,45 @@ wn = sqrt(mld.k/mld.Jb);
 delta = mld.Bb/(2*sqrt(mld.Jb*mld.k));
 w = wn*sqrt(1-delta^2);
 Ts = 0.001;
-
-tk_vector = [];
-thd_k_vector = [];
-current_max = 0;
+%%
 thd_abs = abs(out.thd.signals.values);
 t = out.thd.time;
-for i = thd_abs
-    if i>current_max
-        current_max = i; 
-    elseif i <= 10e-6 
-        thd_k_vector = [thd_k_vector;current_max];
-        index_max = find(thd_abs==current_max);
-        current_t = t(index_max); 
-        tk_vector = [tk_vector; current_t];
-        current_max = 0;
-    end
-        
-end
 
+% tk_vector = [];
+% thd_k_vector = [];
+% 
+%SOL 1
+%  for index = 2:length(thd_abs)-1
+%     if thd_abs(index)>thd_abs(index-1) && thd_abs(index)>thd_abs(index+1)
+%         thd_k_vector = [thd_k_vector thd_abs(index)];
+%         tk_vector = [tk_vector t(index)];
+%     end 
+% end 
+% 
+% SOL 2
+% index = islocalmax(thd_abs);
+% index_peaks = find(index == true & thd_abs > 1);
+% thd_k_vector = thd_abs(index_peaks);
+% tk_vector = t(index_peaks);
 
+% SOL 3 - Signal Processing toolbox needed
+delta_tk = pi / w;
+minimum_samples = round((0.8 * delta_tk) / Ts); % minimum number of samples between to peaks obtained exploiting eqn (68): tk = k*pi-phi/w so t_k+1-t_k = pi/w 
 
-t2 = linspace(0,10,100);
-a=sin(3*t2);
-figure
-plot(t2, a)
+[thd_k_vector, index_peaks] = findpeaks(thd_abs, 'MinPeakHeight', 1,'MinPeakDistance', minimum_samples); % peaks must be at least of 1 deg and at least after tot samples
+tk_vector = t(index_peaks);
+%%
+M = length(thd_k_vector);
+Y = log(thd_k_vector);
+phi = [-(0:M-1)',ones(M,1)];
 
-%tk = linspace
+theta_LS = phi\Y;
+eta_est = theta_LS(1);
 
+delta_est = eta_est/sqrt(pi^2+eta_est^2);
 
+Tk = tk_vector(2:M) - tk_vector(1:M-1);
+wk_est = pi./Tk;
 
-
-%Bb_est=mld.Jb*(2*delta_est*w_n_est);
-%k_est=mld.Jb*w_n_est^2;
+w_est = mean(wk_est);
+wn_est = w_est/sqrt(1-delta_est^2);

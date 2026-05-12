306a84d036aab1d9181272e0dfbe1eb86d477aa7
777gioi <gioidelprato@gmail.com>
2026-05-12 19:58:15 +0200

    UpdateLAB4

--- CM_LAB4/CM_LAB4.m
+++ CM_LAB4/CM_LAB4.m
@@ -3,22 +3,22 @@ clear all
 clc
 
 load ('./../balrob_params.mat'); % robot parameters
+load ('./../param.mat','pulse2deg'); % il prof non l'ha messo nella lista sopra
 
-% Lagrangian Dynamic Equation: matrix formulation
-r=sqrt((2.20*2)/1.06); %non trovo questo parametro nella lista del prof, l'ho ricavato io dall'Handout
+%% Lagrangian Dynamic Equation: matrix formulation
 % M(q)
-M11 = 2*wheel.Iyy + 2*gbox.N^2*mot.rot.Iyy+(batt.m+2*wheel.m+2*mot.m)*r^2;
+M11 = 2*wheel.Iyy + 2*gbox.N^2*mot.rot.Iyy+(body.m+2*wheel.m+2*mot.rot.m)*wheel.r^2;
 M21_1 = 2*gbox.N*(1-gbox.N)*mot.rot.Iyy;
-M21_2 = (batt.m*46.05+2*mot.rot.m*-7)*r;
-M22 = batt.Iyy+2*(1-gbox.N)^2*mot.rot.Iyy+batt.m*46.05^2+2*mot.rot.m*49;
+M21_2 = (body.m*body.zb+2*mot.rot.m*mot.rot.zb)*wheel.r;
+M22 = body.Iyy+2*(1-gbox.N)^2*mot.rot.Iyy+body.m*(body.zb^2)+2*mot.rot.m*(mot.rot.zb^2);
 % C(q,dot_q)
 C11 = 0;
-C12 = -(batt.m*46.05+2*mot.rot.m*-7)*r;
+C12 = -(body.m*body.zb+2*mot.rot.m*mot.rot.zb)*wheel.r;
 % Fv
-F11 = 2*26.5*10^-3;
-F12 = -2*25*10^-3;
-F22 = 2*25*10^-3;
+F11 = 2*(gbox.B+wheel.B);
+F12 = -2*gbox.B;
+F22 = 2*gbox.B;
 F=[F11 F12; F12 F22];
 % g(q)
 G1 = 0;
-G2 = -(batt.m*46.05+2*mot.rot.m*-7)*g;
+G2 = -(body.m*body.zb+2*mot.rot.m*mot.rot.zb)*g;
